fee469ff2e347192651419bcaba404cea94202e7
Yifei <yifei0314@gmail.com>
2023-06-29 15:05:58 -0700

    Update ortega.py

--- ortega/ortega.py
+++ ortega/ortega.py
@@ -343,6 +343,12 @@ class ORTEGA:
         self.speed_average = speed_average
         # self.kernel = kernel
         self.__validate()
+
+        # Check time overlap and lag, stop initialization if conditions are not met
+        if not self.check_time_lag_and_overlap():
+            print(f"Skipping pair {self.id1} and {self.id2} due to time lag greater than {self.minute_max_delay}")
+            return
+        
         self.__start()
     '''
     @property
@@ -490,6 +496,33 @@ class ORTEGA:
             else:
                 self.df1 = self.data[self.data[self.id_field] == self.id1]
                 self.df2 = self.data[self.data[self.id_field] == self.id2]
+                
+
+    def check_time_lag_and_overlap(self):
+        """
+        Check time overlap and time lag between the movements of two individuals (private method, only can be called inside the class).
+        Returns a boolean value indicating whether the time lag is less than or equal to the allowable maximum delay (True) or not (False).
+        """
+        # Extract the start and end times for each individual
+        start1, end1 = self.df1[self.time_field].min(), self.df1[self.time_field].max()
+        start2, end2 = self.df2[self.time_field].min(), self.df2[self.time_field].max()
+
+        # Calculate the time overlap
+        overlap_start = max(start1, start2)
+        overlap_end = min(end1, end2)
+        overlap_duration = (overlap_end - overlap_start).total_seconds() / 60
+
+        # Check if there is no overlap
+        if overlap_duration <= 0:
+            # Calculate the time lag
+            time_lag = abs((start2 - end1).total_seconds() / 60) if end1 < start2 else abs((start1 - end2).total_seconds() / 60)
+
+            # Check if time lag is greater than the max delay
+            if time_lag > self.minute_max_delay:
+                return False
+
+        return True
+    
 
     def __start(self):
         """
