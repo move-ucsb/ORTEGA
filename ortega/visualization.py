4beb8195f2b28bb71efd9792ad6b979425653f32
rongxiangsu <rongxiangsu@gmail.com>
2024-02-15 08:38:30 -0500

    update vis

--- ortega/visualization.py
+++ ortega/visualization.py
@@ -5,30 +5,30 @@ from .ortega import ORTEGA
 from .ellipses import Ellipse
 from typing import List, Tuple
 import matplotlib.patches as mpatches
+import contextily as cx
 
 
-def plot_original_tracks(interation: ORTEGA, save_plot: bool = False, colors=["#ff0000", "#0000ff"]):
+def plot_original_tracks(interation: ORTEGA, save_plot: bool = False, colors=["red", "blue"]):
     """
     Visualize the original movement data with PPAs
+    :param figsize:
     :param colors:
     :param interation:
     :param save_plot:
     :return:
     """
-    # colors = ["#ff0000", "#0000ff"]
-    fig = plt.figure(1, figsize=(8, 8), dpi=90)
+    fig, ax = plt.subplots(1)
     for i, collection in enumerate([interation.ellipses_list_id1, interation.ellipses_list_id2]):
         for item in collection:
             # if throw_out_big_ellipses and item.el[0].length > max_val:
             #     continue
             x, y = item.el.xy
+            # PLOT THE ELLIPSES
             plt.plot(y, x, color=colors[i], alpha=0.5, linewidth=1, solid_capstyle="round")
             # PLOT THE POINTS USED TO MAKE THE ELLIPSES, TOO
             plt.plot([item.lon, item.last_lon], [item.lat, item.last_lat], "o-", color="grey", linewidth=0.5,
                      alpha=0.5, markersize=1)
-    plt.xlabel("X", fontsize=14)
-    plt.ylabel("Y", fontsize=14)
-    plt.grid(True)
+    cx.add_basemap(ax)
     plt.legend(handles=[mpatches.Patch(color=colors[0], label=interation.id1),
                         mpatches.Patch(color=colors[1], label=interation.id2)])
     if save_plot:
@@ -37,7 +37,7 @@ def plot_original_tracks(interation: ORTEGA, save_plot: bool = False, colors=["#
 
 
 def plot_interaction(interation: ORTEGA, all_intersection_pairs: List[Tuple[Ellipse, Ellipse]],
-                     save_plot: bool = False, colors=["#ff0000", "#0000ff", "#f2ff00"]):
+                     save_plot: bool = False, colors=["red", "blue", "yellow"]):
     """
     Visualize interaction. The PPAs of two moving individuals are shown using red and blue ellipses, respectively.
     PPA intersections marking potential interactions are highlighted using yellow ellipses.
@@ -47,14 +47,14 @@ def plot_interaction(interation: ORTEGA, all_intersection_pairs: List[Tuple[Elli
     :param save_plot:
     :return:
     """
-    # colors = ["#ff0000", "#0000ff"]
-    # interaction_color = "#f2ff00"  # yellow
-    fig = plt.figure(1, figsize=(8, 8), dpi=90)
+    # interaction_color = "yellow"
+    fig, ax = plt.subplots(1)
     for i, collection in enumerate([interation.ellipses_list_id1, interation.ellipses_list_id2]):
         for item in collection:
             # if throw_out_big_ellipses and item.el[0].length > max_val:
             #     continue
             x, y = item.el.xy
+            # PLOT THE ELLIPSES
             plt.plot(y, x, color=colors[i], alpha=0.5, linewidth=1, solid_capstyle="round")
             # PLOT THE POINTS USED TO MAKE THE ELLIPSES, TOO
             plt.plot([item.lon, item.last_lon], [item.lat, item.last_lat], "o-", color="grey", linewidth=0.5,
@@ -63,9 +63,7 @@ def plot_interaction(interation: ORTEGA, all_intersection_pairs: List[Tuple[Elli
         for item in two_item:
             x1, y1 = item.el.xy
             plt.plot(y1, x1, color=colors[-1], alpha=0.5, linewidth=1, solid_capstyle="round")
-    plt.xlabel("X", fontsize=14)
-    plt.ylabel("Y", fontsize=14)
-    plt.grid(True)
+    cx.add_basemap(ax)
     plt.legend(handles=[mpatches.Patch(color=colors[0], label=interation.id1),
                         mpatches.Patch(color=colors[1], label=interation.id2)])
     if save_plot:
